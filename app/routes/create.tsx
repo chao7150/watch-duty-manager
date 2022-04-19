6e5750070f3430711a330a4480aae95de52195be
chao <22640673+chao7150@users.noreply.github.com>
2022-04-20 02:25:32 +0900

    feat(create): バリデーション

--- app/routes/create.tsx
+++ app/routes/create.tsx
@@ -1,16 +1,35 @@
 import { Work } from "@prisma/client";
-import { ActionFunction, Form, useActionData } from "remix";
+import { ActionFunction, Form, json, useActionData } from "remix";
 import { db } from "~/utils/db.server";
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
-  const values = Object.fromEntries(formData);
-  const result = await db.work.create({ data: values });
-  return result;
+  const title = formData.get("title");
+  if (typeof title !== "string") {
+    return json({ errorMessage: "title must not be empty" }, { status: 400 });
+  }
+  const optionalWorkCreateInput = [
+    "officialSiteUrl",
+    "twitterId",
+    "hashtag",
+  ].reduce((acc, key) => {
+    const value = formData.get(key) ?? undefined;
+    return { ...acc, [key]: value };
+  }, {} as { officialSiteUrl?: string; twitterId?: string; hashtag?: string });
+  try {
+    const work = await db.work.create({
+      data: { title, ...optionalWorkCreateInput },
+    });
+    return json(work, { status: 200 });
+  } catch {
+    return json({ errorMessage: "the title already exists" }, { status: 409 });
+  }
 };
 
 export default function Create() {
   const actionData = useActionData<Work>();
+  // TODO actionDataってステータスコード読めないの！？
+
   return (
     <>
       <p>

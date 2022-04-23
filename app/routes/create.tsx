b8a08475fca7ccfa028b6f395bfda521e85f4171
chao <22640673+chao7150@users.noreply.github.com>
2022-04-23 19:55:41 +0900

    feat(create): エラー処理を細かくした

--- app/routes/create.tsx
+++ app/routes/create.tsx
@@ -1,19 +1,31 @@
-import { Work } from "@prisma/client";
 import { ActionFunction, Form, json, useActionData } from "remix";
 import { db } from "~/utils/db.server";
+
+const errorCode = {
+  EMPTY_TITLE: "EmptyTitle",
+  WORK_ALREADY_EXISTS: "WorkAlreadyExists",
+};
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
   const title = formData.get("title");
-  if (typeof title !== "string") {
-    return json({ errorMessage: "title must not be empty" }, { status: 400 });
+  if (typeof title !== "string" || title === "") {
+    return json(
+      {
+        errorCode: errorCode.EMPTY_TITLE,
+        errorMessage: "title must not be empty",
+      },
+      { status: 400 }
+    );
   }
   const optionalWorkCreateInput = [
     "officialSiteUrl",
     "twitterId",
     "hashtag",
   ].reduce((acc, key) => {
-    const value = formData.get(key) ?? undefined;
+    const formDataEntryValue = formData.get(key);
+    const value =
+      typeof formDataEntryValue === "string" ? formDataEntryValue : undefined;
     return { ...acc, [key]: value };
   }, {} as { officialSiteUrl?: string; twitterId?: string; hashtag?: string });
   try {
@@ -22,26 +34,63 @@ export const action: ActionFunction = async ({ request }) => {
     });
     return json(work, { status: 200 });
   } catch {
-    return json({ errorMessage: "the title already exists" }, { status: 409 });
+    return json(
+      {
+        errorCode: errorCode.WORK_ALREADY_EXISTS,
+        errorMessage: `${title} already exists`,
+      },
+      { status: 409 }
+    );
   }
 };
 
 export default function Create() {
-  const actionData = useActionData<Work>();
-  // TODO actionDataってステータスコード読めないの！？
+  const actionData = useActionData();
 
   return (
     <>
       <p>
-        {actionData ? `${actionData.title} was successfully submitted.` : ""}
+        {actionData &&
+          (actionData?.errorMessage ??
+            `${actionData.title} was successfully submitted.`)}
       </p>
       <div>
         <Form method="post">
-          <input type="text" name="title" />
-          <input type="text" name="officialSiteUrl" />
-          <input type="text" name="twitterId" />
-          <input type="text" name="hashtag" />
-          <button type="submit">submit</button>
+          <p>
+            Required fields are followed by <abbr title="required">*</abbr>.
+          </p>
+          <ul>
+            <li>
+              <label>
+                title
+                <abbr title="required" aria-label="required">
+                  *
+                </abbr>
+                <input type="text" name="title" />
+              </label>
+            </li>
+            <li>
+              <label>
+                officialSiteUrl
+                <input type="text" name="officialSiteUrl" />
+              </label>
+            </li>
+            <li>
+              <label>
+                twitterId
+                <input type="text" name="twitterId" />
+              </label>
+            </li>
+            <li>
+              <label>
+                hashtag
+                <input type="text" name="hashtag" />
+              </label>
+            </li>
+            <li>
+              <button type="submit">submit</button>
+            </li>
+          </ul>
         </Form>
       </div>
     </>

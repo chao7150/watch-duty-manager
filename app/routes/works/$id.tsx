12912da409fd06afd6b38b464a671b01725cd071
chao <22640673+chao7150@users.noreply.github.com>
2022-05-01 23:59:42 +0900

    feat(subscribe): できる

--- app/routes/works/$id.tsx
+++ app/routes/works/$id.tsx
@@ -2,27 +2,73 @@ import { Prisma, Work } from "@prisma/client";
 import { useCallback, useState } from "react";
 import {
   ActionFunction,
+  Form,
   json,
   LoaderFunction,
+  redirect,
   useActionData,
   useLoaderData,
 } from "remix";
 import { db } from "~/utils/db.server";
 
 import * as WorkCreateForm from "../../components/WorkCreateForm";
+import { getSession } from "~/session";
 
-export const loader: LoaderFunction = async ({ params }) => {
+export const loader: LoaderFunction = async ({ request, params }) => {
+  const session = await getSession(request.headers.get("Cookie"));
+  if (!session.has("uid")) {
+    return redirect("/");
+  }
+  const userId = session.get("uid");
+  if (typeof userId !== "string") {
+    return redirect("/");
+  }
   const id = params.id;
   if (id === undefined) {
     // TODO エラー
     return null;
   }
-  const work = await db.work.findUnique({ where: { id: parseInt(id, 10) } });
-  return work;
+  const work = await db.work.findUnique({
+    where: { id: parseInt(id, 10) },
+    include: { users: { where: { userId } } },
+  });
+  return { ...work, userd: undefined, subscribed: work?.users.length === 1 };
 };
 
 export const action: ActionFunction = async ({ request, params }) => {
+  const session = await getSession(request.headers.get("Cookie"));
+  if (!session.has("uid")) {
+    return redirect("/");
+  }
+  const userId = session.get("uid");
+  if (typeof userId !== "string") {
+    return redirect("/");
+  }
+  const workId = params.id;
+  if (typeof workId !== "string") {
+    return;
+  }
+
   const formData = await request.formData();
+  if (formData.get("_action") === "unsubscribe") {
+    await db.subscribedWorksOnUser.delete({
+      where: { userId_workId: { userId, workId: parseInt(workId, 10) } },
+    });
+    return json({}, { status: 200 });
+  }
+  if (formData.get("_action") === "subscribe") {
+    try {
+      const rel = await db.subscribedWorksOnUser.create({
+        data: {
+          userId,
+          workId: parseInt(workId, 10),
+        },
+      });
+      return json(rel, { status: 200 });
+    } catch (e) {
+      return json({ errorMessage: "db error" }, { status: 400 });
+    }
+  }
   let work: Prisma.WorkCreateInput;
   try {
     work = WorkCreateForm.serverValidator(formData);
@@ -52,7 +98,10 @@ export const action: ActionFunction = async ({ request, params }) => {
   }
 };
 
-type ClientWork = Omit<Work, "publishedAt"> & { publishedAt: string };
+type ClientWork = Omit<Work, "publishedAt"> & {
+  publishedAt: string;
+  subscribed: boolean;
+};
 
 export default function Work() {
   const [editMode, setEditMode] = useState(false);
@@ -78,22 +127,35 @@ export default function Work() {
       {editMode ? (
         <WorkCreateForm.Component {...defaultValueMap} />
       ) : (
-        <dl>
-          <dt>公式サイト</dt>
-          <dd>
-            <a href={work.officialSiteUrl ?? undefined}>
-              {work.officialSiteUrl}
-            </a>
-          </dd>
-          <dt>公式ツイッター</dt>
-          <dd>
-            <a href={`https://twitter.com/${work.twitterId}`}>
-              {work.twitterId}
-            </a>
-          </dd>
-          <dt>ハッシュタグ</dt>
-          <dd>#{work.hashtag}</dd>
-        </dl>
+        <>
+          <dl>
+            <dt>公式サイト</dt>
+            <dd>
+              <a href={work.officialSiteUrl ?? undefined}>
+                {work.officialSiteUrl}
+              </a>
+            </dd>
+            <dt>公式ツイッター</dt>
+            <dd>
+              <a href={`https://twitter.com/${work.twitterId}`}>
+                {work.twitterId}
+              </a>
+            </dd>
+            <dt>ハッシュタグ</dt>
+            <dd>#{work.hashtag}</dd>
+          </dl>
+          <Form method="post">
+            {work.subscribed ? (
+              <button name="_action" value="unsubscribe">
+                unsubscribe
+              </button>
+            ) : (
+              <button name="_action" value="subscribe">
+                subscribe
+              </button>
+            )}
+          </Form>
+        </>
       )}
     </div>
   );
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,27 @@
 import { PrismaClient } from "@prisma/client";
+import { db } from "~/utils/db.server";
 // const db = new PrismaClient();
 
 async function seed() {
-  console.log("not implemented");
+  await db.work.createMany({
+    data: [
+      {
+        title: "test1",
+        publishedAt: new Date("2022-04-01"),
+        officialSiteUrl: "https://example.com",
+      },
+      {
+        title: "test2",
+        publishedAt: new Date("2022-01-01"),
+        twitterId: "TwitterMediaJP",
+      },
+      {
+        title: "test3",
+        publishedAt: new Date("2022-07-01"),
+        hashtag: "ドライブマイカー ",
+      },
+    ],
+  });
 }
 
 seed();

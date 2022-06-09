d6388ffc872c1bdabbede1944616a95cc8e7e77e
chao <22640673+chao7150@users.noreply.github.com>
2022-06-10 03:08:22 +0900

    refactor(create): functional化

--- app/routes/create.tsx
+++ app/routes/create.tsx
@@ -1,46 +1,61 @@
-import { Prisma } from "@prisma/client";
-import { isLeft } from "fp-ts/lib/Either";
+import * as T from "fp-ts/Task";
+import * as TE from "fp-ts/TaskEither";
 import { pipe } from "fp-ts/lib/function";
-import { ActionFunction, json, useActionData } from "remix";
+import { json, useActionData } from "remix";
 import { db } from "~/utils/db.server";
 
 import * as WorkCreateForm from "../components/WorkCreateForm";
+import { DataFunctionArgs } from "@remix-run/server-runtime";
 
-export const action: ActionFunction = async ({ request }) => {
+type ActionData = { title: string } | Response;
+
+export const action = async ({
+  request,
+}: DataFunctionArgs): Promise<ActionData> => {
   const formData = await request.formData();
-  const e = pipe(formData, WorkCreateForm.serverValidator);
-  if (isLeft(e)) {
-    return e.left;
-  }
-  const { episodeCount, ...work } = e.right;
-  try {
-    const returnedWork = await db.work.create({
-      data: work,
-    });
-    const data = Array.from({ length: episodeCount }).map((_, index) => {
-      return {
-        workId: returnedWork.id,
-        count: index + 1,
-        publishedAt: new Date(
-          returnedWork.publishedAt.getTime() + 1000 * 60 * 60 * 24 * 7 * index
-        ),
-      };
-    });
-    await db.episode.createMany({
-      data,
-    });
-    return json(returnedWork, { status: 200 });
-  } catch (e) {
-    return json(
-      {
-        errorMessage: `work already exists`,
-      },
-      { status: 409 }
-    );
-  }
+  return await pipe(
+    formData,
+    WorkCreateForm.serverValidator,
+    TE.fromEither,
+    TE.chain(({ episodeCount, ...work }) => {
+      return TE.tryCatch(
+        async () => ({
+          returnedWork: await db.work.create({ data: work }),
+          episodeCount,
+        }),
+        (e) => json({ errorMessage: "work already exists" }, { status: 409 })
+      );
+    }),
+    TE.chain(({ returnedWork, episodeCount }) => {
+      return TE.tryCatch(
+        async () => {
+          await db.episode.createMany({
+            data: Array.from({ length: episodeCount }).map((_, index) => {
+              return {
+                workId: returnedWork.id,
+                count: index + 1,
+                publishedAt: new Date(
+                  returnedWork.publishedAt.getTime() +
+                    1000 * 60 * 60 * 24 * 7 * index
+                ),
+              };
+            }),
+          });
+          return returnedWork;
+        },
+        (e) =>
+          json({ errorMessage: "episode creation failed" }, { status: 500 })
+      );
+    }),
+    TE.foldW(
+      (e) => T.of(e),
+      (v) => T.of({ ...v })
+    )
+  )();
 };
 
 export default function Create() {
+  // HACK: 型付けろ
   const actionData = useActionData();
 
   return (

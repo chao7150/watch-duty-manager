d88046b9f81aae2453ad9805aa9b34f6be9553d8
chao <22640673+chao7150@users.noreply.github.com>
2022-07-02 02:23:41 +0900

    feat(work): workリストからsubscribeできる

--- app/routes/works/index.tsx
+++ app/routes/works/index.tsx
@@ -1,23 +1,49 @@
-import { Work } from "@prisma/client";
-import { Link, LoaderFunction, useLoaderData } from "remix";
+import { Work, SubscribedWorksOnUser } from "@prisma/client";
+import { DataFunctionArgs } from "@remix-run/server-runtime";
+import { Form, Link, useFetcher, useLoaderData } from "remix";
 import { db } from "~/utils/db.server";
+import { getUserId } from "~/utils/session.server";
 
-export const loader: LoaderFunction = async () => {
+type LoaderData = {
+  works: (Work & { users: SubscribedWorksOnUser[] })[];
+  loggedIn: boolean;
+};
+
+export const loader = async ({
+  request,
+}: DataFunctionArgs): Promise<LoaderData> => {
+  const userId = (await getUserId(request)) ?? undefined;
   const works = await db.work.findMany({
-    select: { id: true, title: true },
+    include: { users: { where: { userId } } },
     orderBy: { id: "asc" },
   });
-  return works;
+  return { works, loggedIn: userId !== undefined };
 };
 
 export default function Works() {
-  const works = useLoaderData<Work[]>();
+  const { works, loggedIn } = useLoaderData<LoaderData>();
+  const fetcher = useFetcher();
   return (
     <ul>
       {works.map((work) => {
         return (
           <li key={work.id}>
-            <Link to={`/works/${work.id}`}>{work.title}</Link>
+            <div className="work-item-row">
+              {loggedIn && (
+                <fetcher.Form method="post" action={`/works/${work.id}`}>
+                  {work.users.length === 1 ? (
+                    <button name="_action" value="unsubscribe">
+                      unsubscribe
+                    </button>
+                  ) : (
+                    <button name="_action" value="subscribe">
+                      subscribe
+                    </button>
+                  )}
+                </fetcher.Form>
+              )}
+              <Link to={`/works/${work.id}`}>{work.title}</Link>
+            </div>
           </li>
         );
       })}

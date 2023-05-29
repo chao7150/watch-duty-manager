15eae84c3281bb18ffb0be35eac0b65d3954ca62
chao7150 <22640673+chao7150@users.noreply.github.com>
2023-05-30 02:31:21 +0900

    feat(index): タイトル絞り込み機能を廃止

--- app/components/Episode/List.tsx
+++ app/components/Episode/List.tsx
@@ -9,23 +9,10 @@ export const Component: React.FC<Props> = ({ episodes }) => {
   const [filterKeyword, setFilterKeyword] = useState<string>("");
   return (
     <div>
-      <input
-        type="search"
-        value={filterKeyword}
-        onChange={(e) => setFilterKeyword(e.target.value)}
-        placeholder="タイトルで絞り込み"
-      />
       <ul className="flex flex-col gap-4 episode-list mt-4">
         {episodes.map((e) => {
-          const hide = !e.title
-            .toLowerCase()
-            .includes(filterKeyword.toLowerCase());
           return (
-            <li
-              className={hide ? "hidden" : ""}
-              key={`${e.workId}-${e.count}`}
-              aria-hidden={hide}
-            >
+            <li key={`${e.workId}-${e.count}`}>
               <Episode.Component {...e} />
             </li>
           );

3cc2639b9a953786b9100dcf20165a571fe5f4f3
chao7150 <22640673+chao7150@users.noreply.github.com>
2025-05-13 21:25:22 +0900

    feat: faviconを変更

--- app/root.tsx
+++ app/root.tsx
@@ -89,6 +89,10 @@ function Document({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>📜</text></svg>"
+        ></link>
         {title ? <title>{title}</title> : null}
         <Meta />
         <Links />
